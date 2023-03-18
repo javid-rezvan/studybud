1681f6694311ce245a93c94a062815c407079edf
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-03-18 23:43:00 +0330

    second config

--- base/views.py
+++ base/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from . models import Room,Topic,Message
 
 def home(request):
-    context={}
+    topics=Topic.objects.all()
+    rooms=Room.objects.all()
+    room_messages=Message.objects.all()
+    context={'topics':topics,'rooms':rooms,'room_messages':room_messages}
     return render(request,'base/home.html',context)
