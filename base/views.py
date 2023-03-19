3c1ed41e08c65af78b686db50a77e88639877447
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-03-19 23:30:21 +0330

    Update views.py

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . models import Room,Topic,Message
 from  django.db.models import Q
+from  .forms import RoomForm
 
 def home(request):
     q=request.GET.get('q') if request.GET.get('q')!=None else ''
@@ -11,3 +12,39 @@ def home(request):
     room_messages=Message.objects.filter(room__topic__name__icontains=q)
     context={'topics':topics,'rooms':rooms,'room_messages':room_messages}
     return render(request,'base/home.html',context)
+
+def room(request,pk):
+    room=Room.objects.get(id=pk)
+    # room_messages=Message.objects.filter(room__name=room)
+    room_messages=room.message_set.all().order_by('-created')
+    participants=room.participants.all()
+    if request.method=='POST':
+        message=Message.objects.create(
+            room=room,
+            user=request.user,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+    
+    context={'room':room,'room_messages':room_messages,'participants':participants}
+    return render(request,'base/room.html',context)
+
+def createRoom(request):
+    form=RoomForm()
+    if request.method=='POST':
+        form=RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+            
+    context={'form':form}
+    return render(request,'base/room_form.html',context)
+    
+    
+def updateRoom(request):
+    context={}
+    return render(request,'base/room_form.html',context)
+
+def deleteRoom(request):
+    context={}
+    return render(request,'base/delete.html',context)    
