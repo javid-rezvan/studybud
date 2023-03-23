58150321186bad4ba9374aab49ce5293f7f7a32f
javid-rezvan <83187226+javid-rezvan@users.noreply.github.com>
2023-03-23 11:28:37 +0330

    Update admin.py

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from . models import Topic,Room,Message
+from . models import Topic,Room,Message,User
 
 admin.site.register(Topic)
 admin.site.register(Message)
 admin.site.register(Room)
+admin.site.register(User)
 
 # Register your models here.
